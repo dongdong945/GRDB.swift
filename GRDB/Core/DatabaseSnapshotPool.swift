3fea5648c2f57e296cc070ac43551366cc09ed19
DongDong <dd.devtech@gmail.com>
2025-11-08 17:58:27 +0800

    Add sqlcipher Support

--- GRDB/Core/DatabaseSnapshotPool.swift
+++ GRDB/Core/DatabaseSnapshotPool.swift
@@ -2,9 +2,13 @@
 // Import C SQLite functions
 #if SWIFT_PACKAGE
 import GRDBSQLite
-#elseif GRDBCIPHER
+#endif
+
+#if GRDBCIPHER
 import SQLCipher
-#elseif !GRDBCUSTOMSQLITE && !GRDBCIPHER
+#endif
+
+#if !GRDBCUSTOMSQLITE && !GRDBCIPHER
 import SQLite3
 #endif
 
